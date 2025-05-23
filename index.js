b7908e21658404cffe93f78b3d102b3d700c8cce
WasabyDev <kepoxander@gmail.com>
2025-05-23 13:57:02 -0300

    feat: starting in JSON

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ var email = document.querySelector("#exempleInputEmail");
 var password = document.querySelector("#exempleInputPassword");
 var photo = document.querySelector("#exempleInputFile");
 var admin = document.querySelector("#exempleInputaAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+
+fields.forEach(function (field, name) {
+  if (field.name == "gender") {
+    if ((field.name = checked)) {
+      user[(field, name)] = field.value;
+    }
+  } else {
+    user[(field, name)] = field.value;
+  }
+});
