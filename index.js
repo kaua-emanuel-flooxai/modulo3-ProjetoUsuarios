270fa3355655e0f633c6ff58536b07b0077d255a
WasabyDev <kepoxander@gmail.com>
2025-05-23 13:29:10 -0300

    feat: variables and selectors class

---
+++ index.js
@@ -0,0 +1,10 @@
+var name = document.querySelector("#exempleInputName");
+var gender = document.querySelectorAll(
+  "#form-user-create [name=gender]:checked"
+);
+var birth = document.querySelector("#exempleInputName");
+var country = document.querySelector("#exempleInputBirth");
+var email = document.querySelector("#exempleInputEmail");
+var password = document.querySelector("#exempleInputPassword");
+var photo = document.querySelector("#exempleInputFile");
+var admin = document.querySelector("#exempleInputaAdmin");
