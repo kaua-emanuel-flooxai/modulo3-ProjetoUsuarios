c0f9cd0adedae14cfa865ffad5e2f1910d1238b4
WasabyDev <kepoxander@gmail.com>
2025-05-26 09:49:00 -0300

    feat: working with MVC and functions to save and add a new user

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
 var name = document.querySelector("#exempleInputName");
-var gender = document.querySelectorAll(
-  "#form-user-create [name=gender]:checked"
-);
-var birth = document.querySelector("#exempleInputName");
-var country = document.querySelector("#exempleInputBirth");
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exempleInputBirth");
+var country = document.querySelector("#exempleInputCountry");
 var email = document.querySelector("#exempleInputEmail");
 var password = document.querySelector("#exempleInputPassword");
 var photo = document.querySelector("#exempleInputFile");
-var admin = document.querySelector("#exempleInputaAdmin");
+var admin = document.querySelector("#exempleInputAdmin");
 
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function (field, name) {
-  if (field.name == "gender") {
-    if ((field.name = checked)) {
-      user[(field, name)] = field.value;
+fields.forEach(function (field) {
+  if (field.name === "gender") {
+    if (field.checked) {
+      user["gender"] = field.value;
     }
+  } else if (field.type === "checkbox") {
+    user[field.name] = field.checked;
   } else {
-    user[(field, name)] = field.value;
+    user[field.name] = field.value;
   }
 });
+
+function addLine(dataUser) {
+  document.getElementById("table-users").innerHTML = `
+    <td>
+      <img
+        src="dist/img/user1-128x128.jpg"
+        alt="User Image"
+        class="img-circle img-sm"
+      />
+    </td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.admin ? "Sim" : "Não"}</td>
+    <td>${dataUser.birth}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+  `;
+}
+
+document
+  .getElementById("form-user-create")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var formData = {};
+    fields.forEach(function (field) {
+      if (field.name === "gender") {
+        if (field.checked) {
+          formData["gender"] = field.value;
+        }
+      } else if (field.type === "checkbox") {
+        formData[field.name] = field.checked;
+      } else {
+        formData[field.name] = field.value;
+      }
+    });
+
+    var objectUser = new User(
+      user.name,
+      user.gender,
+      user.birth,
+      user.country,
+      user.email,
+      user.password,
+      user.photo,
+      user.admin
+    );
+
+    addLine(objectUser);
+  });
