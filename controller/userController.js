0a43797c59f89e726956da96fd2b1f51cf95d9e7
WasabyDev <kepoxander@gmail.com>
2025-05-26 12:37:18 -0300

    feat: validating form

--- controller/userController.js
+++ controller/userController.js
@@ -61,8 +61,16 @@ class UserController {
 
   getValues() {
     let user = {};
+    let isValed = true;
 
     [...this.formEl.elements].forEach(function (field) {
+      if (
+        ["name", "email", "password"].indexOf(field.name) > -1 &&
+        !field.value
+      ) {
+        field.parentElement.classList.add("has-error");
+        isValed = false;
+      }
       if (field.name === "gender") {
         if (field.checked) {
           user["gender"] = field.value;
@@ -73,6 +81,10 @@ class UserController {
         user[field.name] = field.value;
       }
     });
+
+    if (!isValed) {
+      return false;
+    }
 
     return new User(
       user.name,
